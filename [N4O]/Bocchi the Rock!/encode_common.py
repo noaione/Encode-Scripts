aa70e4c46466ff2af0dc83c8425a06516460c302
noaione <noaione0809@gmail.com>
2023-05-26 08:39:32 +0800

    Bocchi: add Vol. 6 (11-12)

--- [N4O]/Bocchi the Rock!/encode_common.py
+++ [N4O]/Bocchi the Rock!/encode_common.py
@@ -35,6 +35,7 @@ from vstools import Matrix, depth, get_prop, get_y, iterate
 
 CURRENT_DIR = VPath(__file__).absolute().parent
 PREMUX_DIR = CURRENT_DIR / "Premux"
+SOURCE_DIR = CURRENT_DIR / "Source"
 PREMUX_DIR.mkdir(exist_ok=True)
 logger = logging.getLogger("encode_common")
 
@@ -44,17 +45,27 @@ def dither_10bit(clip: vs.VideoNode) -> vs.VideoNode:
     return depth(clip, 10).std.Limiter(16 << 2, [235 << 2, 240 << 2], [0, 1, 2])
 
 
+def _open_source_common(
+    open_path: Path, output_file: VPath | Path, trims: tuple[Optional[int], Optional[int]] | None = None
+) -> FileInfo:
+    source = FileInfo(open_path, trims_or_dfs=trims, preset=[PresetBD, PresetOpus])
+    source.name_clip_output = VPath(CURRENT_DIR / output_file.stem)
+    source.set_name_clip_output_ext(".265")
+    return source
+
+
 def open_source(
     name: str, vol: str, output_file: VPath | Path, trims: tuple[Optional[int], Optional[int]] | None = None
 ) -> FileInfo:
     if name.endswith(".m2ts"):
         name = name[:-5]
-    source = FileInfo(
-        CURRENT_DIR / "BDMV" / f"Vol.{vol}" / f"{name}.m2ts", trims_or_dfs=trims, preset=[PresetBD, PresetOpus]
-    )
-    source.name_clip_output = VPath(CURRENT_DIR / output_file.stem)
-    source.set_name_clip_output_ext(".265")
-    return source
+    return _open_source_common(SOURCE_DIR / "BDMV" / f"Vol.{vol}" / f"{name}.m2ts", output_file, trims)
+
+
+def open_web_source(
+    filename: str, output_file: VPath | Path, trims: tuple[Optional[int], Optional[int]] | None = None
+) -> FileInfo:
+    return _open_source_common(SOURCE_DIR / "Web" / filename, output_file, trims)
 
 
 def open_image(path: Path | VPath, ref: vs.VideoNode) -> vs.VideoNode:
@@ -134,7 +145,9 @@ def start_encode(source: FileInfo, clip: vs.VideoNode, *, audio_encoder: AudioEn
         create_keyframes(actual_premux)
         return
     mkv_tracks: list[Track] = [
-        VideoTrack(source.name_clip_output, "Video diolah oleh Suami Nijika-chan | x265 10bit", Lang.make("ja"))
+        VideoTrack(
+            source.name_clip_output, "Video diolah oleh Suami Nijika-chan | x265 10bit", Lang.make("ja")
+        )
     ]
     if source.a_enc_cut is not None:
         mkv_tracks.append(AudioTrack(source.a_enc_cut.set_track(1), "Japanese OPUS 2.0", Lang.make("ja")))
@@ -215,7 +228,9 @@ def LevelsM(
     ...
 
 
-def LevelsM(clip: vs.VideoNode, points: list[float | int], levels: list[int], xpass=[0, "peak"], return_expr=False):
+def LevelsM(
+    clip: vs.VideoNode, points: list[float | int], levels: list[int], xpass=[0, "peak"], return_expr=False
+):
     # https://github.com/Moelancholy/Encode-Scripts/blob/master/Pizza/Urusei%20Yatsura%20(2022)/UY22_09.vpy#L74
     qm = len(points)
     peak = [(1 << clip.format.bits_per_sample) - 1, 1][clip.format.sample_type]
@@ -232,9 +247,7 @@ def LevelsM(clip: vs.VideoNode, points: list[float | int], levels: list[int], xp
         if levels[x] == levels[x + 1]:
             expr += f" {peak * levels[x]} "
         else:
-            expr += (
-                f" x {points[x]} - {peak * (levels[x+1] - levels[x])/(points[x+1] - points[x])} * {peak * levels[x]} + "
-            )
+            expr += f" x {points[x]} - {peak * (levels[x+1] - levels[x])/(points[x+1] - points[x])} * {peak * levels[x]} + "  # noqa
 
     for _ in range(qm):
         expr += " ? "
@@ -260,7 +273,11 @@ def texture_mask(clip: vs.VideoNode, range: int) -> vs.VideoNode:
     em_lo = iterate(em_lo, vs.core.std.Minimum, 2)
     rm_txt = vs.core.std.Expr([rmask, em_hi, em_me, em_lo], "x y z a min min min")
     weighted = LevelsM(
-        rm_txt, points=[x * 256 for x in (1.75, 2.5, 5, 10)], levels=[0, 1, 1, 0], xpass=[0, 0], return_expr=False
+        rm_txt,
+        points=[x * 256 for x in (1.75, 2.5, 5, 10)],
+        levels=[0, 1, 1, 0],
+        xpass=[0, 0],
+        return_expr=False,
     )
 
     masked = weighted.std.BoxBlur(hradius=8, vradius=8).std.Expr(f"x {65535 * 0.2} - {1 / (1 - 0.2)} *")
@@ -270,7 +287,13 @@ def texture_mask(clip: vs.VideoNode, range: int) -> vs.VideoNode:
 def deband_texmask(clip: vs.VideoNode, rady: int = 2, edge: int = 30, edge_dilate: int = 6) -> vs.VideoNode:
     clip_y = get_y(clip)
     tex_mask = texture_mask(clip_y, rady)
-    edgem = core.std.Prewitt(clip_y).std.Binarize(edge << 7).std.Deflate().std.Inflate().morpho.Close(size=edge_dilate)
+    edgem = (
+        core.std.Prewitt(clip_y)
+        .std.Binarize(edge << 7)
+        .std.Deflate()
+        .std.Inflate()
+        .morpho.Close(size=edge_dilate)
+    )
     edgem = core.rgvs.RemoveGrain(edgem, 17)
     return core.std.Expr([tex_mask, edgem], "x y +").rgvs.RemoveGrain(17)
 
